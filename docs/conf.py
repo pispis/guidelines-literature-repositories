1b5d26bb4685114ead1a22f786aea2241e7a8778
Leonidas Pispiringas <lpispir@gmail.com>
2025-07-28 14:43:52 +0300

    Update conf.py
    Playing with the escape characters as the build in readthedocs is failing here.

--- docs/conf.py
+++ docs/conf.py
@@ -211,9 +211,9 @@ latex_elements = {
      			% \includegraphics[height=0.3in]{../../_static/by.png}
           %}
           %}
-          %\\renewcommand{\headrulewidth}{0.4pt}
-           %\\renewcommand{\\footrulewidth}{0.4pt}
-          %\makeatother
+          %\\renewcommand{\\headrulewidth}{0.4pt}
+            %\\renewcommand{\\footrulewidth}{0.4pt}
+          %\\makeatother
            ''',
 # The paper size ('letterpaper' or 'a4paper').
 'papersize': 'a4paper',
